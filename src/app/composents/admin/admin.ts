1bf05069733de3ce20e8ecb0498c868beb8614e2
Shinz0x45 <r.roustan45@gmail.com>
2026-07-22 19:56:31 +0200

    products-table dynamique

--- src/app/composents/admin/admin.ts
+++ src/app/composents/admin/admin.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { UsersTable } from '../users-table/users-table';
 import { ProductsTable } from '../products-table/products-table';
 import { OrdersTable } from '../orders-table/orders-table';
 @Component({
   selector: 'app-admin',
-  imports: [RouterLink, UsersTable, ProductsTable, OrdersTable],
+  imports: [UsersTable, ProductsTable, OrdersTable],
   templateUrl: './admin.html',
   styleUrl: './admin.css',
 })
--- src/app/composents/products-table/products-table.ts
+++ src/app/composents/products-table/products-table.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   templateUrl: './products-table.html',
   styleUrl: './products-table.css',
 })
-export class ProductsTable {}
+export class ProductsTable {
+  products: any = [
+    { id: 1, nom: 'Chemise', description: 'Description du produit Chemise', prix: 15 },
+    { id: 2, nom: 'T-shirt', description: 'Description du produit T-shirt', prix: 10 },
+    { id: 3, nom: 'Jeans', description: 'Description du produit Jeans', prix: 20 }
+  ];
+}
