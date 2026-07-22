d6b4496609d320712026f917e69a79fb22a1b4b5
Shinz0x45 <r.roustan45@gmail.com>
2026-07-22 19:21:31 +0200

    add users&products table

--- src/app/composents/admin/admin.ts
+++ src/app/composents/admin/admin.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
+import { UsersTable } from '../users-table/users-table';
+import { ProductsTable } from '../products-table/products-table';
+import { OrdersTable } from '../orders-table/orders-table';
 @Component({
   selector: 'app-admin',
-  imports: [RouterLink],
+  imports: [RouterLink, UsersTable, ProductsTable, OrdersTable],
   templateUrl: './admin.html',
   styleUrl: './admin.css',
 })
---
+++ src/app/composents/products-table/products-table.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-table',
+  imports: [],
+  templateUrl: './products-table.html',
+  styleUrl: './products-table.css',
+})
+export class ProductsTable {}
