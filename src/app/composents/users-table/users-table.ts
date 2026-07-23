3f254137cda620dd3dc2fecf71be1117506b4ee8
Shinz0x45 <r.roustan45@gmail.com>
2026-07-23 09:49:37 +0200

    users-table dynamique

--- src/app/composents/users-table/users-table.ts
+++ src/app/composents/users-table/users-table.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './users-table.html',
   styleUrl: './users-table.css',
 })
-export class UsersTable {}
+export class UsersTable {
+
+  users: any = [
+    {id :  1, nom : 'Roustan', prenom : 'Rémi', mail : 'r.roustan@test.com', role : 'Admin'},
+    {id :  2, nom : 'Sellier', prenom : 'Maurice', mail : 'm.sellier@test.com', role : 'Client'},
+    {id :  3, nom : 'Islem', prenom : 'Ben Mabrouk', mail : 'b.mabrouk@test.com', role : 'Client'},
+
+  ]
+
+
+}
