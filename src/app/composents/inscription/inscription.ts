93f71120cae9acf5aa05c9ed38fe6c2c077dc1b3
Shinz0x45 <r.roustan45@gmail.com>
2026-07-23 12:56:42 +0200

    inscription dynamique, ajout de msg d'erreur, sauvrage dans localstorage

--- src/app/composents/inscription/inscription.ts
+++ src/app/composents/inscription/inscription.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-inscription',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './inscription.html',
   styleUrl: './inscription.css',
 })
-export class Inscription {}
+export class Inscription {
+  inscriptionForm!: FormGroup;
+  users: any[] = [];
+
+  // constructor(private formBuilder: FormBuilder) {}
+  private formBuilder = inject(FormBuilder);
+
+  ngOnInit(): void {
+    this.inscriptionForm = this.formBuilder.group({
+      nom: ['', [Validators.required, Validators.minLength(3)]],
+      prenom: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      mdp: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]],
+      confirmMdp: ['', [Validators.required]],
+      genre: ['', Validators.required],
+      vetements: [false],
+      accessoires: [false],
+      pays: ['', Validators.required]
+    });
+  }
+
+  inscription() {
+    const formValue = this.inscriptionForm.value;
+    let interets = [];
+    if (formValue.vetements) interets.push('Vêtements');
+    if (formValue.accessoires) interets.push('Accessoires');
+    
+    const userFinal = {
+      nom: formValue.nom,
+      prenom: formValue.prenom,
+      email: formValue.email,
+      mdp: formValue.mdp,
+      genre: formValue.genre,
+      interets: interets,
+      pays: formValue.pays
+    }
+    this.users = JSON.parse(localStorage.getItem('users') || '[]');
+    this.users.push(userFinal);
+    localStorage.setItem('users', JSON.stringify(this.users));
+    alert('Inscription réussie !');
+  }
+
+}
